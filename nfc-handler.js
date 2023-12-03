1b0fe8dbf404b20cc1516ce1576e4846150acba3
Chu35 <A109070003@mail.shu.edu.tw>
2023-12-04 03:12:16 +0800

    a

--- nfc-handler.js
+++ nfc-handler.js
@@ -1,3 +1,47 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const questions = document.querySelectorAll('.nfc .nfcpart');
+    let currentQuestionIndex = 0;
+
+    function showCurrentQuestion() {
+        questions.forEach((question, index) => {
+            if (index === currentQuestionIndex) {
+                question.style.display = 'block';
+            } else {
+                question.style.display = 'none';
+            }
+        });
+    }
+
+    function nextQuestion() {
+        if (currentQuestionIndex < questions.length - 1) {
+            currentQuestionIndex++;
+            showCurrentQuestion();
+        }
+    }
+
+    function prevQuestion() {
+        if (currentQuestionIndex > 0) {
+            currentQuestionIndex--;
+            showCurrentQuestion();
+        }
+    }
+
+    // Event listeners for next and previous buttons
+    const nextButtons = document.querySelectorAll('.nfc .next-button');
+    nextButtons.forEach(button => {
+        button.addEventListener('click', nextQuestion);
+    });
+
+    const prevButtons = document.querySelectorAll('.nfc .prev-button');
+    prevButtons.forEach(button => {
+        button.addEventListener('click', prevQuestion);
+    });
+
+    showCurrentQuestion();
+});
+
+
+
 $(document).ready(function () {
     function handleNFCTagScanned(element) {
         // 获取当前元素的 YouTube 视频 ID

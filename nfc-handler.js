34bc04ba96afb15a61a3b353218c71ad8534f1e8
Chu35 <A109070003@mail.shu.edu.tw>
2023-12-04 02:43:28 +0800

    a

--- nfc-handler.js
+++ nfc-handler.js
@@ -1,22 +1,30 @@
 $(document).ready(function () {
-    function handleNFCTagScanned() {
-        // 在這裡添加播放音樂的代碼s
-        // 這裡使用了 YouTube 嵌入式播放器
-        var youtubeVideoId = 'Dnj5Tcpev0Q';
-        var iframeSrc = 'https://www.youtube.com/embed/' + youtubeVideoId + '?autoplay=1';
+    function handleNFCTagScanned(element) {
+        // 获取当前元素的 YouTube 视频链接
+        var youtubeLink = $(element).data('youtube-link');
 
-        // 创建一个iframe并嵌入YouTube视频
-        var iframe = $('<iframe width="560" height="315" src="' + iframeSrc + '" frameborder="0" allowfullscreen></iframe>');
+        // 检查是否存在有效的 YouTube 视频链接
+        if (youtubeLink) {
+            // 创建一个iframe并嵌入YouTube视频
+            var iframeSrc = youtubeLink.replace('watch?v=', 'embed/') + '?autoplay=1';
+            var iframe = $('<iframe width="560" height="315" src="' + iframeSrc + '" frameborder="0" allowfullscreen></iframe>');
 
-        // 将iframe追加到页面中
-        $('.nfcpart').append(iframe);
+            // 将iframe追加到当前元素中
+            $(element).append(iframe);
 
-        console.log("NFC標籤已掃描，播放 YouTube 視頻...");
+            console.log("NFC標籤已掃描，播放 YouTube 視頻...");
+        }
     }
 
     // 監聽NFC事件
     $('.nfcpart').on('click', function (event) {
         event.preventDefault();
-        handleNFCTagScanned();
+        handleNFCTagScanned(this);
+    });
+
+    // 如果有“聽完了”按钮，也可以添加相应的点击事件
+    $('.finish-button').on('click', function (event) {
+        // 处理聽完了按钮的点击事件
+        console.log("聽完了按钮被点击...");
     });
 });

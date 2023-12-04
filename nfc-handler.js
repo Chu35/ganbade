6d841c98b95021439cf65fda722906c880533c99
Chu35 <A109070003@mail.shu.edu.tw>
2023-12-04 13:19:59 +0800

    a

--- nfc-handler.js
+++ nfc-handler.js
@@ -1,30 +1,35 @@
-$(document).ready(function () {
-    function handleNFCTagScanned(youtubeLink) {
-        // 检查是否存在有效的 YouTube 视频链接
-        if (youtubeLink) {
-            // 创建一个iframe并嵌入YouTube视频
-            var iframeSrc = youtubeLink.replace('watch?v=', 'embed/') + '?autoplay=1';
-            var iframe = $('<iframe width="560" height="315" src="' + iframeSrc + '" frameborder="0" allowfullscreen></iframe>');
-
-            // 将iframe追加到页面中
-            $('.nfcpart').empty().append(iframe);
-
-            console.log("NFC标签已扫描，播放 YouTube 视频...");
+document.addEventListener('DOMContentLoaded', function () {
+    const questions = document.querySelectorAll('.nfc .nfcpart');
+    let currentQuestionIndex = 0;
+    function showCurrentQuestion() {
+        questions.forEach((question, index) => {
+            if (index === currentQuestionIndex) {
+                question.style.display = 'block';
+            } else {
+                question.style.display = 'none';
+            }
+        });
+    }
+    function nextQuestion() {
+        if (currentQuestionIndex < questions.length - 1) {
+            currentQuestionIndex++;
+            showCurrentQuestion();
         }
     }
-
-    // 模拟 NFC 标签扫描事件
-    $('#simulate-scan').on('click', function () {
-        // 假设这里使用了 nfc-tools 获取了新的 NFC 标签的信息
-        var newYoutubeLink = prompt("请输入新的 YouTube 链接：");
-
-        // 调用处理 NFC 标签扫描的函数
-        handleNFCTagScanned(newYoutubeLink);
+    function prevQuestion() {
+        if (currentQuestionIndex > 0) {
+            currentQuestionIndex--;
+            showCurrentQuestion();
+        }
+    }
+    // Event listeners for next and previous buttons
+    const nextButtons = document.querySelectorAll('.nfc .next-button');
+    nextButtons.forEach(button => {
+        button.addEventListener('click', nextQuestion);
     });
-
-    // 如果有“聽完了”按钮，也可以添加相应的点击事件
-    $('.finish-button').on('click', function (event) {
-        // 处理聽完了按钮的点击事件
-        console.log("聽完了按钮被点击...");
+    const prevButtons = document.querySelectorAll('.nfc .prev-button');
+    prevButtons.forEach(button => {
+        button.addEventListener('click', prevQuestion);
     });
+    showCurrentQuestion();
 });
